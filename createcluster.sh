93612f3065562f93cdadb79eec2f04738d47e871
Erik de Jonge <erik@a8.nl>
2015-03-19 11:47:30 +0100

    consoleprinter Thursday 19 March 2015 (week:11 day:78), 11:47:27

--- createcluster.sh
+++ createcluster.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 sudo date
+
 function _killallpythonprocs() {
   killall python3 2> /dev/null;  killall Python 2> /dev/null;  killall python3 2> /dev/null;  killall Python 2> /dev/null;  killall pypy 2> /dev/null;  killall python3 2> /dev/null;  killall Python3 2> /dev/null;  killall python3 2> /dev/null;  killall Python3 2> /dev/null;  killall pypy3 2> /dev/null;
 }
 _killallpythonprocs
+
 rm -f hosts
 rm -f ./configscripts/user-data*
 
